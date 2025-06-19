bab1183bdfdbf3dd13b54eb81f13d79d24f4c3bc
whimsical5 <jadhavatharva28@gmail.com>
2025-06-18 23:53:44 -0700

    Fix: safe migration using AppConfig.ready()

---
+++ startups/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'startups.apps.StartupsConfig'
--- startups/apps.py
+++ startups/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+import os
 
 class StartupsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'startups'
+
+    def ready(self):
+        if os.environ.get('RUN_MIGRATE_ON_DEPLOY') == 'true':
+            from django.core.management import call_command
+            try:
+                call_command('migrate')
+                call_command('collectstatic', interactive=False, verbosity=0)
+            except Exception as e:
+                print("Migration error:", e)
