22b5d320646793c835ea487ef099d5cd82c670d3
whimsical5 <jadhavatharva28@gmail.com>
2025-06-19 14:22:33 -0700

    Fix: enable public-read ACL for S3 uploads

--- startup_graveyard/settings.py
+++ startup_graveyard/settings.py
@@ -197,3 +197,5 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
+DEFAULT_ACL = 'public-read'
